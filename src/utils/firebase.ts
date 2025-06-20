7dd9e96d09cccd2a5cb4c9f7da974c14537b55ca
rZn24h <razvanhertanu@gmail.com>
2025-06-20 11:47:09 +0300

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCngBpxC_rH2I5Cp4qiVKv4imSHXi2tHY",
-  authDomain: "exam-scheduler-5c99a.firebaseapp.com",
-  projectId: "exam-scheduler-5c99a",
-  storageBucket: "exam-scheduler-5c99a.firebasestorage.app",
-  messagingSenderId: "983680589183",
-  appId: "1:983680589183:web:c35a3839105c75f3afce4a",
-  measurementId: "G-BFKWHL08VZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDCngBpxC_rH2I5Cp4qiVKv4imSHXi2tHY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "exam-scheduler-5c99a.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "exam-scheduler-5c99a",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "exam-scheduler-5c99a.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "983680589183",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:983680589183:web:c35a3839105c75f3afce4a",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-BFKWHL08VZ"
 };
 
 // Initialize Firebase
